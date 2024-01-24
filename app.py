f3c59c10f6a288e1e7daa45bca9d93b7b9f8f354
ddubeyyy <134207381+ddubeyyy@users.noreply.github.com>
2024-01-24 14:04:29 +0000

    First Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, render_template, request
+import pickle
+import sklearn
+
+tokenizer=pickle.load(open("models/cv.pkl","rb"))
+model=pickle.load(open("models/clf.pkl","rb"))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if request.method=='POST':
+        email = request.form.get('content')
+    tokenized_email = tokenizer.transform([email]) # X 
+    prediction = model.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return render_template("index.html", prediction=prediction, email=email)
+
+if __name__=="__main__":
+    app.run(debug=True)
